c5b9d018d5980375e2af084f6aea3a5be33132fb
Clayton Goff <cltngoff@gmail.com>
2021-03-06 22:18:30 -0800

    buttons and current date added

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var currentDate = moment().format("dddd, MMMM Do");
+// var text1 = document.getElementById('textArea').value;     // plain JavaScript
+// var text2 = $("#myTextArea").val();
+
+// set currentDate on page
+$("#currentDay").append(currentDate);
+
+var saveTasks = function () {
+    
+};
+
+// capture the button click
+$("button").on("click", function() {
+   // console.log("The button was clicked");
+  });
+
